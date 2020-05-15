edf60af504e02eb154b51745504d928e00b39e20
ezdyveg <Corban.Stevens@McKesson.com>
2020-05-15 13:18:53 -0700

    Initial commit for user homework

---
+++ user-crud/src/test/java/com/corban/upskilling/homework/user/usercrud/UserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corban.upskilling.homework.user.usercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
